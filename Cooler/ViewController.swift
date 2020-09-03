147631e179a63047ef0886b8d51f2966380cd16e
Levi Saltzman <levisaltzman@gmail.com>
2020-09-02 21:44:30 -0400

    Initial Commit

---
+++ Cooler/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cooler
+//
+//  Created by Levi Saltzman on 9/2/20.
+//  Copyright © 2020 Levi Saltzman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
