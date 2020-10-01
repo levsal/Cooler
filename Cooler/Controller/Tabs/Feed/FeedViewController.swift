cfd820deea1ac764973acad7fa4481a77f0dcc0e
Levi Saltzman <levisaltzman@gmail.com>
2020-09-30 23:19:49 -0400

    Add FriendsPostsTableViewCells

--- Cooler/Controller/Tabs/Feed/FeedViewController.swift
+++ Cooler/Controller/Tabs/Feed/FeedViewController.swift
@@ -14,14 +14,17 @@ class FeedViewController: UIViewController {
     let db = Firestore.firestore()
     
     var friends : [String] = []
-
+    
     @IBOutlet weak var feedTableView: UITableView!
     
- 
+    var tableViewScanner = 0
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         feedTableView.register(UINib(nibName: "AddFriendsTableViewCell", bundle: nil), forCellReuseIdentifier: "AddFriendsTableViewCell")
+        feedTableView.register(UINib(nibName: "FriendsPostsTableViewCell", bundle: nil), forCellReuseIdentifier: "FriendsPostsTableViewCell")
         feedTableView.dataSource = self
         
         //Create Friends List
@@ -51,18 +54,25 @@ class FeedViewController: UIViewController {
     }
     
     
-
+    
 }
 
 extension FeedViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 5
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "AddFriendsTableViewCell", for: indexPath) as! AddFriendsTableViewCell
-        cell.parentVC = self
-        return cell
+        if tableViewScanner == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "AddFriendsTableViewCell", for: indexPath) as! AddFriendsTableViewCell
+            cell.parentVC = self
+            tableViewScanner += 1
+            return cell
+        }
+        else{
+            let cell = tableView.dequeueReusableCell(withIdentifier: "FriendsPostsTableViewCell", for: indexPath) as! FriendsPostsTableViewCell
+            return cell
+        }
     }
     
     
---
+++ Cooler/Controller/Tabs/Feed/FriendsPostsCell/FriendsPostsTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  FriendsPostsTableViewCell.swift
+//  Cooler
+//
+//  Created by Levi Saltzman on 9/30/20.
+//  Copyright © 2020 Levi Saltzman. All rights reserved.
+//
+
+import UIKit
+
+class FriendsPostsTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var friendsPostTextView: UITextView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        friendsPostTextView.layer.borderWidth = 1
+        friendsPostTextView.layer.borderColor = UIColor.red.cgColor
+        friendsPostTextView.layer.cornerRadius = friendsPostTextView.layer.frame.width/40
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
